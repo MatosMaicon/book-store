fa6ed30f7ecfc989a57955aaeae9f4f1e00b4d1c
Gilles Lima <gillestoim@gmail.com>
2019-05-15 15:58:21 -0300

    implement mini-cart and update layout

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'reactstrap';
+import { NavLink, Card } from 'reactstrap';
 
-const Cart = ({ products }) => (
-  <NavLink>
-    <span>Carrinho <strong>{products.length ? products.length : 0}</strong></span>
-  </NavLink>
-);
+import './style.css';
 
-export default connect(state => ({ products: state.cart }))(Cart);
+class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleMouseHover = this.handleMouseHover.bind(this);
+    this.state = {
+      isHovering: false,
+    };
+  }
+
+  handleMouseHover() {
+    this.setState(this.toggleHoverState);
+  }
+
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <NavLink
+          onMouseEnter={this.handleMouseHover}
+          onMouseLeave={this.handleMouseHover}>
+          <span>Carrinho <strong>{this.props.products.length ? this.props.products.length : 0}</strong></span>
+        </NavLink>
+        {
+          this.state.isHovering &&
+          <Card className="mini-cart">
+            <ul className="list-unstyled">
+              {
+                this.props.products.length ? 
+                  this.props.products.map(product => 
+                  <li>{product.qty}x - {product.name} - R${product.price}</li>
+                  ) : 
+                  <li>Seu carrinho está vazio.</li>
+              }
+            </ul>
+          </Card>
+        }
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  products: state.cart,
+  props: ownProps
+})
+
+export default connect(mapStateToProps)(Cart);
