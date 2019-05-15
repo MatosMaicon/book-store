8bc1df622bb424edd542f6fd664c52e24fa9b2f2
Gilles Lima <gillestoim@gmail.com>
2019-05-15 14:43:05 -0300

    update cart reducer

--- frontend/src/store/reducers/cart.js
+++ frontend/src/store/reducers/cart.js
@@ -2,12 +2,17 @@ export default function cart(state = [], action) {
 
   switch (action.type) {
     case 'ADD_PRODUCT_CART':
-      if (state.includes(action.product)) {
-        return state
+      const duplicate = state.filter(product => product.id === action.product.id);
+      if (!!duplicate.length) {
+        const nproduct = duplicate[0];
+        const newstate = state.filter(product => product.id !== action.product.id);
+
+        nproduct.qty += 1;
+        return [ ...newstate, nproduct ];
       }
-      return [ ...state, action.product ]
+      return [ ...state, action.product ];
     default:
-      return state
+      return state;
   }
 
 }
