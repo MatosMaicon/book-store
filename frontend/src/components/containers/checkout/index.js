c78e0c6cb2df2f2f967e26bd6539afc496ba99f8
Gilles Lima <gillestoim@gmail.com>
2019-05-27 16:38:42 -0300

    create checkout component

---
+++ frontend/src/components/containers/checkout/index.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Container, Button } from 'reactstrap';
+import { toastr } from 'react-redux-toastr';
+
+import * as ActionsCart from '../../../store/actions/cart';
+import Api from '../../../services/orders';
+import { getDecodedToken } from '../../../services/token';
+import './style.css';
+
+const Checkout = ({ products, ownProps, removeProductToCart, removeAllProducts }) => {
+
+  const handleSubmitOrder = async (e) => {
+    e.preventDefault();
+
+    const token = getDecodedToken();
+    const orders = products.map(item => ({
+      bookId: item.id,
+      quantity: item.qty
+    }));
+
+    try {
+      const response = await Api.save({
+        dateOrder: new Date().toISOString(),
+        status: 'pending',
+        items: orders,
+        total: products.reduce((acc, item) => acc + (item.price * item.qty), 0),
+        userId: token.id
+      })
+
+      if (response) {
+        toastr.success('Sucesso', 'Operação Realizada com sucesso.');
+        removeAllProducts();
+        ownProps.history.push('/client');
+      }
+    } catch (error) {
+      console.log(error);
+      toastr.error('Error', `${error}`);
+    }
+    
+  }
+  return (
+    <>
+      <Container className="mt-4">
+        <h5>Produtos</h5>
+        <ul className="list-unstyled">
+        {
+          products.length ? 
+            products.map(product => 
+            <li
+              key={product.id}
+              className="checkout-list-prods">
+                <Button
+                  outline
+                  size="sm"
+                  color="danger"
+                  onClick={() => removeProductToCart(product.id)}>
+                <i className="fa fa-trash"></i></Button>
+                <div
+                  className="checkout-list-item"
+                  style={{width: '100%', marginLeft: '10px'}}>
+                  <span>{product.qty}x</span>
+                  <Link to={`/product/${product.id}/detail`} className="truncate">{product.name}</Link>
+                  <span>R${product.price}</span>
+                </div>
+            </li>
+            ) : 
+            <li>Seu carrinho está vazio.</li>
+        }
+          <li className="mt-3">
+            <div className="checkout-list-prods">
+              <span>Total</span>
+              <span>
+                <strong>R$ {
+                  products.length ? 
+                    products.reduce((acc, item) => acc + (item.price * item.qty), 0) 
+                    : '0.00'
+                }</strong>
+              </span>
+            </div>
+          </li>
+        </ul>
+        <Button
+          color="success"
+          onClick={handleSubmitOrder}
+          disabled={!products.length}
+        >Finalizar</Button>
+      </Container>
+    </>
+  );
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  products: state.cart,
+  ownProps
+})
+
+const mapDispatchToProps = dispatch => ({
+  removeProductToCart: id => dispatch(ActionsCart.removeProductToCart(id)),
+  removeAllProducts: () => dispatch(ActionsCart.removeAllProducts()) 
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
