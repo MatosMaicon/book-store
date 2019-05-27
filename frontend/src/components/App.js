ad27e52608958622beeea06d1c8e104f2cfb374b
Gilles Lima <gillestoim@gmail.com>
2019-05-27 16:39:01 -0300

    add new route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,6 +14,7 @@ import BookForm from './book-form';
 import OrderList from './order-list';
 import Auth from './auth';
 import Message from './shared/message'
+import Checkout from './containers/checkout';
 
 import './App.css';
 
@@ -27,9 +28,9 @@ const App = props => {
           <Route exact path='/login' component={Auth} />
 
           <Route exact path='/product/:id/detail' component={ProductDetail} />
+          <Route exact path='/checkout' component={Checkout} />
 
           <ProtectedRoute exact path='/client' component={OrderList} rule="admin" />
-
           <ProtectedRoute exact path='/books' component={BookList} rule="admin" />
           <ProtectedRoute exact path='/books/new' component={BookForm} rule="admin" />
           <ProtectedRoute exact path='/books/edit/:id' component={BookForm} rule="admin" />
