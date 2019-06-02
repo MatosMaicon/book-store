2c029faf512d56109e67da086f1eec7d1421d3f9
Maicon <maicon.dev@gmail.com>
2019-06-02 16:45:22 -0300

    improvement test integration backend

--- backend/expressjs/__tests__/factories.js
+++ backend/expressjs/__tests__/factories.js
@@ -1,7 +1,7 @@
 const faker = require("faker");
 const random = require("./utils/random")
 const { factory } = require("factory-girl");
-const { User } = require("../src/app/models");
+const { User, Order, Product } = require("../src/app/models");
 
 factory.define("User", User, {
   name: faker.name.findName(),
@@ -9,6 +9,22 @@ factory.define("User", User, {
   password: faker.internet.password(),
   userType: random(0,1) ? 'admin' : 'client',
   active: !random(0,1)
+});
+
+factory.define("Order", Order, {
+  dateOrder: faker.date.recent(),
+  userId: 1,
+  status: 'pedding',
+  total: 500,
+  itens: [{bookId: 1, quantity: random(1,5)}]
+  
+});
+
+factory.define("Product", Product, {
+  name: faker.name.title(),
+  description: faker.lorem.paragraph(),
+  price: faker.random.number({min: 1, max:250, precision: 0.1}),
+  image: faker.image.business()
 });
 
 module.exports = factory;
--- backend/expressjs/__tests__/integration/order.test.js
+++ backend/expressjs/__tests__/integration/order.test.js
@@ -5,15 +5,32 @@ const truncate = require("../utils/truncate");
 const factory = require("../factories");
 
 describe("Order", () => {
+
+  generateOrder = async (qtd) => {
+    let user, order = null
+
+    const product = await factory.create("Product");
+
+    for(let i=0; i<qtd; i++){
+      user = await factory.create("User");
+      console.log(user.email) //email duplicado
+  
+      order = await factory.create("Order", {
+        userId: user.id
+      });
+    }
+
+    const token = await user.generateToken()
+
+    return { order: order, user: user, token: token }
+  }
+
   beforeEach(async () => {
     await truncate();
   });
 
   it("should be able to list orders when authenticated", async () => {
-    const user = await factory.create("User", {
-      password: "123123"
-    });
-
+    user = await factory.create("User");
     const token = await user.generateToken()
 
     const response = await request(app)
@@ -35,5 +52,20 @@ describe("Order", () => {
       .set("Authorization", `Bearer 123123`);
 
     expect(response.status).toBe(401);
+  });
+
+  it("should be able to show order when authenticated", async () => {
+    const generate = await generateOrder(1)
+
+    const response = await request(app)
+      .get(`${process.env.APP_URI}/orders/${generate.order.id}`)
+      .set("Authorization", `Bearer ${generate.token}`);
+
+    expect(response.status).toBe(200);
+
+    expect([response.body])
+      .toContainEqual(
+        expect.objectContaining({ dateOrder: generate.order.dateOrder, total: expect.anything() })
+      );
   });
 });
